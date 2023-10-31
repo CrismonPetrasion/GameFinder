894ec80402074b7db0830100d5d04010cd12e7a7
CrismonPetrasion <148696199+CrismonPetrasion@users.noreply.github.com>
2023-10-31 19:02:29 +0300

    GameFinder.lua

---
+++ GameFinder.lua
@@ -0,0 +1,5 @@
+if game:IsLoaded() then
+  if game.PlaceId == 13772394625 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/CrismonPetrasion/BladeBall.lua/main/BladeBall.lua"))()
+  end
+end
